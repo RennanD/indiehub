5f21e8b3e9783417af2713f2d2f1cde7e8f4c8d7
RennanD <rplaykl@gmail.com>
2025-12-13 16:24:09 -0300

    feat: add beta tester on checktou succesfuly

--- src/templates/app/onboarding-page/sections/bio-step/bio-step.tsx
+++ src/templates/app/onboarding-page/sections/bio-step/bio-step.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
+
 import { forwardRef, useImperativeHandle, useState } from "react";
 import { useController, useForm } from "react-hook-form";
 import { updateBio } from "@/actions/update-bio";
